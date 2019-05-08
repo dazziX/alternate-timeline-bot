52baaf6b03049f0fff7b79cc7f3dddc58a0b07e7
dazziX <47928800+dazziX@users.noreply.github.com>
2019-05-09 02:38:31 +0800

    Update trivia.py

--- fake/trivia.py
+++ fake/trivia.py
@@ -8,6 +8,7 @@ import textwrap
 import markovify
 
 
+
 def get_trivia(month: int=None,day: int=None):
     if not month or not day:
         now = date.today()
@@ -37,24 +38,32 @@ def generate_image(trivia, query):
     im = Image.open(BytesIO(raw))
     #width, height = im.size
     new = ImageOps.fit(im, (500,500), Image.LANCZOS, 0.0, (0.5,0.5))
-    black_box = Image.new('RGB',(500,700),(10,0,0))
-    black_box.paste(new, (0,0))
-    final = add_text(trivia, black_box)
-    return {'final':final,'old':im,'new':new}
+    #black_box = Image.new('RGB',(500,700),(10,0,0))
+    #black_box.paste(new, (0,0))
+    final = add_text(trivia, new)
+    return {'out':final,'unsplash':im}
 
 
 def add_text(text,img):
     width,height = img.size
     lines = textwrap.wrap(text, width=42)
-    draw = ImageDraw.Draw(img)
     font = ImageFont.truetype('times-new-roman.ttf',25)
-    space = 0
+    text_height = 42
     for line in lines:
         fw,fh = font.getsize(line)
-        draw.text(((width - fw) // 2, 515 + space), line, font=font, fill=(255,255,255))
-        space += fh + 4
+        text_height += fh + 4
+
+    text_box = Image.new('RGB',(500,height + text_height),(10,0,0))
+    text_box.paste(img, (0,0))
+    draw = ImageDraw.Draw(text_box)
+
+    text_height = 0
+    for line in lines:
+        fw,fh = font.getsize(line)
+        draw.text(((width - fw) // 2, 520 + text_height), line, font=font, fill=(255,255,255))
+        text_height += fh + 4
         
-    return img
+    return text_box
 
 
 
@@ -64,7 +73,6 @@ if __name__ == '__main__':
     print('-'*20)
     print(trivia['query'])
     img = generate_image(trivia['text'],trivia['query'])
-    img['final'].save('final.png')
+    img['out'].save('out.png')
     
     
-
